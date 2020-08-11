1685bb888dd4c2244937baa7a4a9689bce454bf3
mesinger <uintptr96@gmail.com>
2020-08-11 18:22:46 +0200

    Setup for nuke build

---
+++ build/Build.cs
@@ -0,0 +1,50 @@
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Aws);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+
+    AbsolutePath Src => RootDirectory / "src";
+    AbsolutePath Test => RootDirectory / "test";
+
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+            DotNetClean();
+            
+            Src.GlobDirectories("**/bin", "**/obj").ForEach(d =>
+            {
+                EnsureCleanDirectory(d);
+                DeleteDirectory(d);
+            });
+            
+            Test.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+        });
+
+    Target Aws => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+
+        });
+}
