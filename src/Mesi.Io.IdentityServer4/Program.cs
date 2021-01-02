8e2588a4d76d9ef102fe30caaf67d9cd99c7a70e
mesi <uintptr96@gmail.com>
2021-01-02 22:57:50 +0100

    Update program entry point to work with ef core

--- src/Mesi.Io.IdentityServer4/Program.cs
+++ src/Mesi.Io.IdentityServer4/Program.cs
@@ -2,30 +2,36 @@
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
-using Mesi.Io.IdentityServer4;
 using Microsoft.Extensions.Configuration;
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Month)
-    .CreateLogger();
-try
+namespace Mesi.Io.IdentityServer4
 {
-    Log.Information("Starting host...");
-    CreateHostBuilder(args).Build().Run();
-    return 0;
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Month)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting host...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+        
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config => { config.AddEnvironmentVariables("MESI_IO_IDENTITY_SERVER_"); })
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
 }
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Host terminated unexpectedly.");
-    return 1;
-}
-finally
-{
-    Log.CloseAndFlush();
-}
-
-static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureAppConfiguration(config => { config.AddEnvironmentVariables("MESI_IO_IDENTITY_SERVER_"); })
-        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
